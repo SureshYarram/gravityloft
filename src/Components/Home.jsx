12f05e9515764c9743264796669c846a64888175
SureshYarram <sureshchowdary540@gmail.com>
2022-05-24 12:13:01 +0530

    landing page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,7 @@
 import search from "../Images/Search 3.png"
 import Graphics from "../Images/graphics.png"
+import Logo from "../Images/Logo.png"
+import img from "../Images/img.png"
 
 export const Home = ()=>{
 
@@ -46,8 +48,18 @@ to find experts</p>
              </div>
          </div>
          <div className="childdiv">
-             <div className="left"></div>
-             <div className="right"></div>
+             <div className="right ">
+                 <img className="img" src={img} alt="image" />
+             </div>
+             <div className="left">
+
+             <p className="color">Consult.</p>
+             <p>with Top Industry Experts</p>
+             <p>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+             </p>
+             <p>Get Started</p>
+             </div>
          </div>
          <div className="childdiv">
          <div className="left">
@@ -65,10 +77,77 @@ to find experts</p>
      </div>
      <p className="text">FAQ</p>
      <p className="loremtext">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-     <div className="faq"></div>
-     <div className="contact"></div>
+     <div className="faq">
+     <div class="divs">
+          <p>What is Techrefic ?</p>  
+        
+           <p>-</p>
+          
+        </div>
+        <div id="block">
+         <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos iure assumenda quam reprehenderit nihil. Aspernatur incidunt ea iure labore laborum ipsa libero harum nostrum, dolorem fuga tempore laboriosam consequuntur facilis.</p>
+         </div>
+        <hr />
+        <div class="divs">
+          <p>What is Techrefic ?</p>  
+          <p>+</p>
+        </div>
+        <hr />
+        <div class="divs">
+          <p>What is Techrefic ?</p>
+          <p>+</p>  
+        </div>
+        <hr />
+        <div class="divs">
+          <p>What is Techrefic ?</p>  
+          <p>+</p>
+        </div>
+        <hr />
+        <div class="divs">
+          <p>What is Techrefic ?</p>  
+          <p>+</p>
+        </div>
+        <hr />
+        <div class="divs">
+          <p>What is Techrefic ?</p>  
+          <p>+</p>
+        </div>
+       
      </div>
-
+     <div className="contact">
+          <p>Contact us, If you facing any problems</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <button id="btn1">Contact Now</button>
+     </div>
+     </div>
+      <div className="footer">
+          <div>
+              <img src={Logo} alt="" />
+              <p id="para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora omnis explicabo soluta. Illum voluptatem.</p>
+          </div>
+          <div>
+              <p>Account</p>
+              <ul>
+                  <li>Login</li>
+                  <li>Signup</li>
+              </ul>
+          </div>
+          <div>
+              <p>Company</p>
+              <ul>
+                  <li>Privacy Policy</li>
+                  <li>Terms of use</li>
+                  <li>Compliance </li>
+              </ul>
+          </div>
+          <div>
+              <p>Support</p>
+              <ul>
+                  <li>Contact Us</li>
+                  <li>FAQ</li>
+              </ul>
+          </div>
+      </div>
 </>
     )
 }
